e919446dd86d551925a1c9293fc39f77138729bd
unknown <rapoluindhu2929@gmail.com>
2024-10-01 22:46:06 +0530

    switch case and while loop task on 1-09-24

---
+++ switch.js
@@ -0,0 +1,85 @@
+// Find day of the week using switch case
+console.log("question -1");
+
+var a="monday";
+switch(a){
+    case "monday":
+        console.log("Its Monday");
+        break;
+        case "tuesday":
+            console.log("Its Tuesday");
+            break;
+            case "wedensday":
+                console.log("Its Wednesday");
+                break;
+                case "thursday":
+                    console.log("Its Thursday");
+                    break;
+                    case "friday":
+                        console.log("Its Friday");
+                        break;
+                        case "satursday":
+                            console.log("Its Saturday");
+                            break;
+                            default:
+                                console.log("Its Sunday");
+                                   
+}
+// Find month of the year using switch case
+console.log("question -2");
+var n=5;
+switch(n){
+    case 1:
+        console.log("Its January");
+        break;
+        case 2:
+            console.log("Its February");
+            break;
+            case 3:
+                console.log("Its March");
+                break;
+                case 4:
+                    console.log("Its April");
+                    break;
+                    case 5:
+                        console.log("Its May")
+                        break;
+                        case 6:
+                            console.log("Its June");
+                            break;
+                            case 7:
+                                console.log("Its July");
+                                break;
+                                case 8:
+                                    console.log("Its August");
+                                    break;
+                                    case 9:
+                                        console.log("Its September");
+                                        break;
+                                        case 10:
+                                            console.log("Its October");
+                                            break;
+                                            case 11:
+                                                console.log("Its November");
+                                                break;
+                                                default:
+                                                console.log("Its December");
+                                                
+}
+// Find even numbers in first 10 numbers using while loop
+console.log("question -3");
+m=0
+while(m<=10){
+    if(m%2==0){
+        console.log(m);
+    }
+    m++
+}
+// Countdown timer using while loop and the countdown should starts from 10
+console.log("question -4");
+
+var b=10;
+while(b>=0){
+    console.log(b);
+    b--
+}
